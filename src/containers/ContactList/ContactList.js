5e6160a71f94522ab75bfbdb3b1261dfb28cc166
Vitalik Kim <kimv1086@gmail.com>
2020-01-25 15:38:21 +0600

    fix

--- src/containers/ContactList/ContactList.js
+++ src/containers/ContactList/ContactList.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import './ContactList.css';
+// import './ContactList.css';
+import {Button, Col, Container, Media} from "reactstrap";
 
 const ContactList = props =>  {
     return (
-        <div className="ContactList">
-            <img className="ContactImage" src={props.image} alt="contact"/>
-            <p>{props.contactName}</p>
-            <button onClick={props.open}>Show more</button>
-        </div>
+        // <div className="ContactList">
+        //     <img className="ContactImage" src={props.image} alt="contact"/>
+        //     <p>{props.contactName}</p>
+        //     <button onClick={props.open}>Show more</button>
+        // </div>
+<Container>
+    <Media>
+        <Media src={props.image +} alt={props.contactName}/>
+        <Col>
+            <Media body>
+                <Media heading>{props.contactName}</Media>
+                <Button outline color="primary" onClick={props.open}>Show more</Button>{' '}
+            </Media>
+        </Col>
+
+    </Media>
+</Container>
+
     );
 };
 
