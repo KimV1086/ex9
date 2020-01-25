d583f1c3dad47c45de9f443bb18f999f9863da6e
Vitalik Kim <kimv1086@gmail.com>
2020-01-25 11:48:44 +0600

    add component main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import MainNav from "./components/MainNav/MainNav";
+
 
 function App() {
   return (
     <div className="App">
-
+      <MainNav/>
     </div>
   );
 }
---
+++ src/components/MainNav/MainNav.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+
+    render() {
+        return (
+            <nav className="MainNav">
+                <h3 className="Logo">Contacts</h3>
+            </nav>
+        );
+    }
+}
+
+export default Nav;
